f5e4813b19b652fdf2b504865bcc0794fbda4708
cubismic <am824@outlook.com>
2023-03-14 07:10:54 +0900

    Solve 02_05 except 6

--- src/02_Basics/05_Proving_Facts_about_Algebraic_Structures.lean
+++ src/02_Basics/05_Proving_Facts_about_Algebraic_Structures.lean
@@ -33,13 +33,165 @@ variables x y z : α
 #check (le_sup_right: y ≤ x ⊔ y)
 #check (sup_le : x ≤ z → y ≤ z → x ⊔ y ≤ z)
 
-example : x ⊓ y = y ⊓ x := sorry
-example : x ⊓ y ⊓ z = x ⊓ (y ⊓ z) := sorry
-example : x ⊔ y = y ⊔ x := sorry
-example : x ⊔ y ⊔ z = x ⊔ (y ⊔ z) := sorry
+example : x ⊓ y = y ⊓ x :=
+begin
+  apply le_antisymm,
+  {
+    show x ⊓ y ≤ y ⊓ x,
+    apply le_inf,
+    apply inf_le_right,
+    apply inf_le_left,
+  },
+  {
+    show y ⊓ x ≤ x ⊓ y,
+    apply le_inf,
+    apply inf_le_right,
+    apply inf_le_left,
+  },
+end
+example : x ⊓ y ⊓ z = x ⊓ (y ⊓ z) :=
+begin
+  apply le_antisymm,
+  {
+    show x ⊓ y ⊓ z ≤ x ⊓ (y ⊓ z),
+    apply le_inf,
+    {
+      show x ⊓ y ⊓ z ≤ x,
+      apply le_trans,
+      apply inf_le_left,
+      apply inf_le_left,
+    },
+    apply le_inf,
+    {
+      show x ⊓ y ⊓ z ≤ y,
+      apply le_trans,
+      apply inf_le_left,
+      apply inf_le_right,
+    },
+    {
+      show x ⊓ y ⊓ z ≤ z,
+      apply le_trans,
+      apply inf_le_right,
+      apply le_refl,
+    },
+  },
+  {
+    show x ⊓ (y ⊓ z) ≤ x ⊓ y ⊓ z,
+    apply le_inf,
+    apply le_inf,
+    {
+      show x ⊓ (y ⊓ z) ≤ x,
+      apply le_trans,
+      apply inf_le_left,
+      apply le_refl,
+    },
+    {
+      show x ⊓ (y ⊓ z) ≤ y,
+      apply le_trans,
+      apply inf_le_right,
+      apply inf_le_left,
+    },
+    {
+      show x ⊓ (y ⊓ z) ≤ z,
+      apply le_trans,
+      apply inf_le_right,
+      apply inf_le_right,
+    }
+  },
+end
+example : x ⊔ y = y ⊔ x :=
+begin
+  apply le_antisymm,
+  {
+    show x ⊔ y ≤ y ⊔ x,
+    apply sup_le,
+    apply le_sup_right,
+    apply le_sup_left,
+  },
+  {
+    show y ⊔ x ≤ x ⊔ y,
+    apply sup_le,
+    apply le_sup_right,
+    apply le_sup_left,
+  },
+end
+example : x ⊔ y ⊔ z = x ⊔ (y ⊔ z) :=
+begin
+  apply le_antisymm,
+  {
+    show x ⊔ y ⊔ z ≤ x ⊔ (y ⊔ z),
+    apply sup_le,
+    {
+      show x ⊔ y ≤ x ⊔ (y ⊔ z),
+      apply sup_le,
+      apply le_sup_left,
+      apply le_trans,
+      apply le_sup_left,
+      exact z,
+      apply le_sup_right,
+    },
+    {
+      show z ≤ x ⊔ (y ⊔ z),
+      apply le_trans,
+      apply le_sup_right,
+      exact y,
+      apply le_sup_right,
+    },
+  },
+  {
+    show x ⊔ (y ⊔ z) ≤ x ⊔ y ⊔ z,
+    apply sup_le,
+    {
+      show x ≤ x ⊔ y ⊔ z,
+      apply le_trans,
+      apply le_sup_left,
+      exact y,
+      apply le_sup_left,
+    },
+    apply sup_le,
+    {
+      show y ≤ x ⊔ y ⊔ z,
+      apply le_trans,
+      apply le_sup_right,
+      exact x,
+      apply le_sup_left,
+    },
+    {
+      show z ≤ x ⊔ y ⊔ z,
+      apply le_sup_right,
+    },
+  },
+end
 
-theorem absorb1 : x ⊓ (x ⊔ y) = x := sorry
-theorem absorb2 : x ⊔ (x ⊓ y) = x := sorry
+theorem absorb1 : x ⊓ (x ⊔ y) = x :=
+begin
+  apply le_antisymm,
+  {
+    show x ⊓ (x ⊔ y) ≤ x,
+    apply inf_le_left,
+  },
+  {
+    show x ≤ x ⊓ (x ⊔ y),
+    apply le_inf,
+    apply le_refl,
+    apply le_sup_left,
+  },
+end
+
+theorem absorb2 : x ⊔ (x ⊓ y) = x :=
+begin
+  apply le_antisymm,
+  {
+    show x ⊔ x ⊓ y ≤ x,
+    apply sup_le,
+    apply le_refl,
+    apply inf_le_left,
+  },
+  {
+    show x ≤ x ⊔ x ⊓ y,
+    apply le_sup_left,
+  },
+end
 
 end
 
