07959bc9fe97bb19e0706b64ee5f4976a9a9afa3
KALOS <90180935+kalos-security@users.noreply.github.com>
2023-05-09 21:10:00 +0900

    5.1

--- src/05_Number_Theory/01_Irrational_Roots.lean
+++ src/05_Number_Theory/01_Irrational_Roots.lean
@@ -51,23 +51,42 @@ example {m n : ℕ} (coprime_mn : m.coprime n) : m^2 ≠ 2 * n^2 :=
 begin
   intro sqr_eq,
   have : 2 ∣ m,
-    sorry,
+    by {apply even_of_even_sqr, rw sqr_eq, norm_num},
   obtain ⟨k, meq⟩ := dvd_iff_exists_eq_mul_left.mp this,
   have : 2 * (2 * k^2) = 2 * n^2,
   { rw [←sqr_eq, meq], ring },
   have : 2 * k^2 = n^2,
-    sorry,
+    by {norm_num at this, assumption},
   have : 2 ∣ n,
-    sorry,
+    by {apply even_of_even_sqr, rw ← this, norm_num},
   have : 2 ∣ m.gcd n,
-    sorry,
+    by { apply nat.dvd_gcd, assumption, assumption },
   have : 2 ∣ 1,
-    sorry,
+    by { rw coprime_mn.gcd_eq_one at this, assumption },
   norm_num at this
 end
 
 example {m n p : ℕ} (coprime_mn : m.coprime n) (prime_p : p.prime) : m^2 ≠ p * n^2 :=
-    sorry
+begin
+  intro sqr_eq,
+  have pdivm2 : p ∣ m,
+    by {apply prime_p.dvd_of_dvd_pow, rw sqr_eq, norm_num},
+  obtain ⟨k, meq⟩ := dvd_iff_exists_eq_mul_left.mp pdivm2,
+  have : p * (p * k^2) = p * n^2,
+    by { rw [←sqr_eq, meq], ring },
+  have : p * k^2 = n^2,
+    by { apply nat.eq_of_mul_eq_mul_left prime_p.pos, rw this },
+  have : p ∣ n,
+    by {apply nat.prime.dvd_of_dvd_pow prime_p, rw ← this, norm_num},
+  have pdivmgcdn: p ∣ m.gcd n,
+    by { apply nat.dvd_gcd, assumption, assumption },
+  have : p ∣ 1,
+    by { rw coprime_mn.gcd_eq_one at pdivmgcdn, assumption },
+  have : 2 ≤ 1,
+    by { apply prime_p.two_le.trans,
+    exact nat.le_of_dvd zero_lt_one this },
+  norm_num at this
+end
 
 #check nat.factors
 #check nat.prime_of_mem_factors
@@ -94,9 +113,16 @@ begin
   have nsqr_nez : n^2 ≠ 0,
     by simpa,
   have eq1 : nat.factorization (m^2) p = 2 * m.factorization p,
-    sorry,
+    by { rw factorization_pow' },
   have eq2 : (p * n^2).factorization p = 2 * n.factorization p + 1,
-    sorry,
+    by {
+      rw factorization_mul',
+      rw factorization_pow',
+      rw prime_p.factorization',
+      ring,
+      exact nat.prime.ne_zero prime_p,
+      exact nsqr_nez
+    },
   have : (2 * m.factorization p) % 2 = (2 * n.factorization p + 1) % 2,
   { rw [←eq1, sqr_eq, eq2] },
   rw [add_comm, nat.add_mul_mod_self_left, nat.mul_mod_right] at this,
@@ -110,14 +136,23 @@ begin
   { simp },
   have npow_nz : n^k ≠ 0 := λ npowz, nnz (pow_eq_zero npowz),
   have eq1 : (m^k).factorization p = k * m.factorization p,
-    sorry,
+    by { rw factorization_pow' },
   have eq2 : (r.succ * n^k).factorization p =
       k * n.factorization p + r.succ.factorization p,
-    sorry,
+    by {
+      rw factorization_mul',
+      rw factorization_pow',
+      ring,
+      exact r.succ_ne_zero,
+      exact npow_nz
+    },
   have : r.succ.factorization p = k * m.factorization p - k * n.factorization p,
   { rw [←eq1, pow_eq, eq2, add_comm, nat.add_sub_cancel] },
   rw this,
-  sorry
+  apply nat.dvd_sub',
+  apply nat.dvd_mul_right,
+  -- ??
+  exact dvd.intro ((nat.factorization n) p) rfl
 end
 
 #check multiplicity
